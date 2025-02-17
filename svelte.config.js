dde12b5016d2a08afa1d8a610ee6e6bd953825c8
Prins <eleazar.klyuvitkin@std.heh.be>
2025-02-17 12:58:02 +0100

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from "@sveltejs/adapter-auto";
+import preprocessor from "svelte-preprocess";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: preprocessor(),
+  kit: {
+    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+    adapter: adapter(),
+  },
+};
+
+export default config;
