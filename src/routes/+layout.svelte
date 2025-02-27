7d865993260206010d0f2cac555b260f37a6b04d
Prins <kltk.eleazar@gmail.com>
2025-02-27 21:12:27 +0100

    add cursor component & install gsap

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script>
 	let { children } = $props();
 	import '../lib/styles/main.scss'; // Import direct du SCSS
+	import Cursor from '../lib/cursor.svelte';
 </script>
 
 {@render children()}
+
+<!-- <Cursor /> -->
 
 <style lang="scss">
 </style>
