296aa8238e82d06126baa1644fb33a4ef5147260
Prins <kltk.eleazar@gmail.com>
2026-04-17 02:00:27 +0200

    add more logs

--- src/routes/template/cafe-des-delices/+page.server.js
+++ src/routes/template/cafe-des-delices/+page.server.js
@@ -7,12 +7,14 @@ async function fetchSection(section, key) {
 	const res = await fetch(`${API_URL}/${ORG_SLUG}/?sections=${section}`, {
 		headers: { Authorization: `Bearer ${key}` }
 	});
+	console.log(res)
 	if (!res.ok) return null;
 	return res.json();
 }
 
 export const load = async () => {
 	const data = await fetchSection('horaires,poles,galerie', CAFE_DELICE);
+	console.log('Données récupérées du CMS:', data);
 	return {
 		horaires: data?.horaires?.data ?? null,
 		poles: data?.poles?.data ?? null,
