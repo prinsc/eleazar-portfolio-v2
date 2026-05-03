6fa8120f020a936497a63c30a4d583d79b0c4cd0
Prins <kltk.eleazar@gmail.com>
2026-05-03 22:32:27 +0200

    refix sitemap

--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	// src/routes/+page.js
-	import { redirect } from '@sveltejs/kit';
-
-	export const load = () => {
-		throw redirect(307, '/fr'); // Redirige vers /fr par défaut
-	};
-</script>
